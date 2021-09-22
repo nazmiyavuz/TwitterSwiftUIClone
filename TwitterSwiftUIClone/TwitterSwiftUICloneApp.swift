e14c65f1f6b6373b3df1974fd26931c4a20a46f9
nazmiyavuz <nazmi_yavuz@hotmail.com>
2021-09-22 09:06:40 +0300

    Initial Commit

---
+++ TwitterSwiftUIClone/TwitterSwiftUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUICloneApp.swift
+//  TwitterSwiftUIClone
+//
+//  Created by Nazmi Yavuz on 22.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
